c53290f44b3aae922e260c3971584402ea522064
Kervin Espirito Santo <57775586+kervines@users.noreply.github.com>
2023-11-01 23:24:42 +0000

    refatoração no code

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const image = document.querySelector('img');
 const header = document.querySelector('.header-container');
 
-const propertyHeader = window.getComputedStyle(header);
-const heightHeader = Math.floor(+propertyHeader.height.replace('px', ''));
+// const propertyHeader = window.getComputedStyle(header);
+// const heightHeader = Math.floor(+propertyHeader.height.replace('px', ''));
+
+const heightHeader = header.scrollHeight;
 
 window.addEventListener('scroll', () => {
-  if (Math.floor(window.scrollY) + heightHeader > image.height) {
+  if (Math.floor(window.scrollY) + header.scrollHeight > image.height) {
     header.classList.add('change-color-header');
   } else {
     header.classList.remove('change-color-header');
