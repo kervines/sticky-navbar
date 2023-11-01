409555a4b47599dc18480b5b5b90017a54519389
Kervin Espirito Santo <57775586+kervines@users.noreply.github.com>
2023-11-01 01:49:12 +0000

    implementação do arquivo `script.js`

---
+++ script.js
@@ -0,0 +1,16 @@
+const image = document.querySelector('img');
+const header = document.querySelector('.header-container');
+
+const propertyHeader = window.getComputedStyle(header);
+const heightHeader = Math.floor(+propertyHeader.height.replace('px', ''));
+
+window.addEventListener('scroll', () => {
+  if (Math.ceil(window.scrollY) + heightHeader > image.height) {
+    header.style.backgroundColor = 'black';
+    header.style.color = 'white';
+    header.style.transition = '0.3s';
+  } else {
+    header.style.backgroundColor = 'white';
+    header.style.color = 'black';
+  }
+});
