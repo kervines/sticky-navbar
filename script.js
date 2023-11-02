12d951b4ad22d953ce7f2f2fad8df45943139e7d
Kervin Espirito Santo <57775586+kervines@users.noreply.github.com>
2023-11-02 23:44:03 +0000

    update `script.js`

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const image = document.querySelector('img');
+const image = document.querySelector('.welcome-main');
 const header = document.querySelector('.header-container');
-
+const links = document.querySelectorAll('a');
 // const propertyHeader = window.getComputedStyle(header);
 // const heightHeader = Math.floor(+propertyHeader.height.replace('px', ''));
 
+const heightImage = image.scrollHeight;
 const heightHeader = header.scrollHeight;
 
 window.addEventListener('scroll', () => {
-  if (Math.floor(window.scrollY) + header.scrollHeight > image.height) {
+  if (Math.floor(window.scrollY) + header.scrollHeight > heightImage) {
     header.classList.add('change-color-header');
+    links.forEach((link) => {
+      link.classList.add('change-color-menu');
+    });
   } else {
     header.classList.remove('change-color-header');
+    links.forEach((link) => {
+      link.classList.remove('change-color-menu');
+    });
   }
 });
